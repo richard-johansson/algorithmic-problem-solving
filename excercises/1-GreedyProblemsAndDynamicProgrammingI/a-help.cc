218423ff9050e419f6c070f1bb0c2d66790081c8
Richard Johansson <ricjo462@student.liu.se>
2022-01-21 11:04:18 +0100

    Started on Help, written pseudocode for solving the problem

--- excercises/1-GreedyProblemsAndDynamicProgrammingI/a-help.cc
+++ excercises/1-GreedyProblemsAndDynamicProgrammingI/a-help.cc
@@ -5,17 +5,29 @@
 
 using namespace std;
 
+/**
+* Compile with g++ -std=c++17 a-help.cc 
+* Run with ./a.out < ../../test.txt 
+*
+*/
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     string line{};
-    while(getline(cin, line))
-    {
 
+    // Read number of test cases
+    // For every test case
+        // read two lines (patterns) into two vectors
+        // loop through each pattern
+            // if there is a placeholder on either line
+                // map placeholders to words
+            // push the words to a stringstream
+        // if the stringstream is not empty
+            // print out the stringstream
+        // else
+            // print '-'
 
-
-    }
     return 0;
 }
