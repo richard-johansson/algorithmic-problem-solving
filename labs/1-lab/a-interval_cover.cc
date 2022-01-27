08a14b43527afad21aeb07c590d9c7a58d16dddc
ricjo462 <ricjo462@su17-117.ad.liu.se>
2022-01-27 10:16:27 +0100

    Started on method to solve the problem

--- labs/1-lab/a-interval_cover.cc
+++ labs/1-lab/a-interval_cover.cc
@@ -7,10 +7,41 @@
  */
 #include <iostream>
 #include <string>
-#include <map>
 #include <utility>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+/**
+ * @brief Finds which sub-intervals that are needed for complete coverage.
+ * 
+ * @param intervals 
+ * @param intervalToCover 
+ * @return vector<int> The index of the needed intervals.
+ */
+vector<int> covering(vector<pair<double, double>> intervals, double start, double end)
+{
+    vector<int> chosenIntervals{0};
+    int chosenIndex{0};
+    double currentStart{start}, currentEnd{start};
+    // Iterating all intervals
+    for (int i{0}; i<intervals.size(); ++i)
+    {
+        // If this interval covers the start of the last interval
+        if(intervals[i].first <= currentStart)
+        {
+            // And if this interval 
+            if (intervals[i].second >= currentEnd)
+            {
+                currentStart = intervals[i].first;
+                currentEnd = intervals[i].second;
+                chosenIndex = i;
+            }
+        }
+    }
+
+}
 
 int main()
 {
@@ -21,11 +52,12 @@ int main()
     double mainStart{}, mainEnd{};
     while(cin >> mainStart >> mainEnd)
     {
+        //pair<double, double> intervalToCover{mainStart,mainEnd};
         // Read the data
         int numberOfIntervals{0};
         cin >> numberOfIntervals;
 
-        multimap<double, double> intervals{};
+        vector<pair<double, double>> intervals{};
         for (int i{0}; i<numberOfIntervals; ++i)
         {
             double subStart{}, subEnd{};
@@ -50,8 +82,12 @@ int main()
             * the end of the interval before and covers most of the remaining 
             * interval.
         */
-        
-
+       vector<int> chosenIntervals = covering(intervals, mainStart, mainEnd);
+       for (auto& intervalIndex : chosenIntervals)
+       {
+           cout << intervalIndex << " ";
+       }
+       cout << endl;
     }
 
 
