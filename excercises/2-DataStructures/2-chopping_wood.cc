a4e3a3db3d26a99d5a3e89ba856047e354c13c85
Richard Johansson <ricjo462@student.liu.se>
2022-02-03 17:58:48 +0100

    Done

--- excercises/2-DataStructures/2-chopping_wood.cc
+++ excercises/2-DataStructures/2-chopping_wood.cc
@@ -20,16 +20,15 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int inputLen{0};
-
     // Read number of test cases
+    int inputLen{0};
     cin >> inputLen;
     
     // Read input to vector
-    int biggestInt{inputLen+1}, input{0};
+    int input{0};
     vector<int> v;
-    // Vector with input as index and 
-    vector<int> v_index(inputLen+2,-1);
+    // Vector with input as index and last found as value
+    vector<int> v_index(inputLen+2, -1);
 
     // For every test case
     for (int i{0}; i<inputLen; ++i)
@@ -38,59 +37,34 @@ int main()
         cin >> input;
         v.push_back(input);
         v_index[input] = i;
-        //cout << "[" << input << "] = " << i-1 << endl;
     }
 
+    // Early return since last element must be deletet last!
     if (v[inputLen-1] != inputLen+1)
     {
         cout << "Error\n";
         return 0;
     }
 
-    for (int i{0}; i<inputLen+2; ++i)
-    {
-        cout << v_index[i] << " ";
-    }
-    cout << endl;
-
     // Save all integers not in v into heap
     priority_queue< int, vector<int>, greater<int> > heap;
     for (int i{1}; i<inputLen+2; ++i)
     {
-        // If not element i is found the element is missing
+        // If element not in v_index, push to heap
         if (v_index[i] == -1)
         {
             heap.push(i);
         }
     }
 
-    // //Print heap
-    // cout << "HEAP: ";
-    // while ( ! heap.empty() )
-    // {
-    //     cout << heap.top() << " ";
-    //     heap.pop();
-    // }
-    // cout << endl;
-
-    // Print v
-    cout << "V: ";
-    for (auto &a : v)
-    {
-        cout << a << " ";
-    }
-    cout << endl;
-
     // Print heap
     for (int i{0}; i<inputLen; ++i)
     {
-        //cout << v.front() << endl;
         // Pop the smallest value of heap
         cout << heap.top() << "\n";
         heap.pop();
 
         // Add first value of v to heap
-        // 
         if (v_index[v[i]] == i)
         {
             heap.push(v[i]);
