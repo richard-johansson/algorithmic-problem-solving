d6224d7094c90673898775a04548a45d14207f5c
Richard Johansson <ricjo462@student.liu.se>
2022-02-09 10:45:04 +0100

    Deleted old solution

--- excercises/3-Arithmetic/a-perfect_pth_powers.cc
+++ excercises/3-Arithmetic/a-perfect_pth_powers.cc
@@ -9,74 +9,11 @@
  */
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <vector>
 #include <cmath>
-#include <numeric>
-#include <unordered_map>
-#include <math.h>
 
 using namespace std;
 
-vector<long int> get_primes(long int x)
-{
-    vector<long int> factor{};
-    unordered_map<long int, long int> primes{};
-
-    cout << "Factors: ";
-
-    // Save all 2s that divide x
-    while (x % 2 == 0)
-    {
-        cout << 2 << " ";
-        primes[2] += 1;
-        x = x/2;
-    }
-
-    // x must be odd at this point. So we can skip
-    // one element (Note i = i + 2)
-    for (long int i{3}; i <= sqrt(x); i = i + 2)
-    {
-        // While i divides n, save i and divide x
-        while (x % i == 0)
-        {
-            cout << i << " ";
-            primes[i] += 1;
-            x = x/i;
-        }
-    }
-
-    // If x is a prime number greater than 2
-    if (x > 2)
-    {
-        cout << x << " ";
-        primes[x] += 1;
-    }
-
-    for (auto &a : primes)
-    {
-        factor.push_back(a.second);
-    }
-    cout << "\n";
-
-    return factor;
-}
-
-long int get_gcd(vector<long int> const& primes)
-{
-    long int result = primes[0];
-
-    cout << "Primes: ";
-    for (long int i{0}; i<primes.size(); ++i)
-    {
-        result = gcd(result, primes[i]);
-        cout << result << " ";
-    }
-    cout << "\n";
-    return result;
-}
-
-long int solution2(long int x)
+long int solve(long int x)
 {
     bool is_negative{false};
     for (long int i{32}; i>0; --i)
@@ -108,10 +45,8 @@ long int solution2(long int x)
         {
             continue;
         }
-
         return i;
     }
-
     return x;
 }
 
@@ -126,29 +61,9 @@ int main()
     {
         if (x == 0) break;
 
-        // if (x < 2 && x > -2)
-        // {
-        //     cout << "Invlalid input: " << x << "\n";
-        //     continue;
-        // }
         // cout << "x: " << x << "\n";
 
-    // Solution 1
-        // // factor n into primes
-        // vector<long int> factor{get_primes(x)};
-        // // x is perfect power iff gcd(factors) > 0
-        // long int gcd{get_gcd(factor)};
-        //
-        // if (gcd > 0)
-        // {
-        //     cout << gcd << "\n";
-        // }
-        // else 
-        // {
-        //     cout << x << "\n";
-        // }
-
-        cout << solution2(x) << endl;;
+        cout << solve(x) << endl;;
     }
 
     return 0;
