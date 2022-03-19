1d3f4fe6211e4d24951e0d074f503abe5e5b653c
Richard Johansson <ricjo462@student.liu.se>
2022-03-19 16:44:23 +0100

    Working for given tests, with test prints

---
+++ excercises/7-GraphsIII/b-hiding_places.cc
@@ -0,0 +1,128 @@
+/**
+ * @file b-hiding_places.cc
+ * @author Richard Johansson (ricjo462@student.liu.se)
+ * @brief 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool inbounds(int x, int y)
+{
+    return (x >= 0 && x < 8 && y >= 0 && y < 8);
+}
+
+void bfs(vector<vector<int>> &distances, int x_start, int y_start, vector<int> &dx, vector<int> &dy)
+{
+    queue<pair<int,int>> q;
+    q.push(make_pair(x_start, y_start));
+    distances[x_start][y_start] = 0;
+
+    // Test print
+    // cout << "|" << setfill('-') << setw(8*13+1) << "|\n" << setfill(' ');
+    // for (auto x : distances) {
+    //     for (auto y : x) {
+    //         cout << "|" << setw(11) << y << " ";
+    //     }
+    //     cout << "|\n|" << setfill('-') << setw(8*13+1) << "|\n" << setfill(' ');
+    // }
+
+    // BFS traversal
+    while (!q.empty())
+    {
+        int x_curr = q.front().first;
+        int y_curr = q.front().second;
+        q.pop();
+
+        for (int i{0}; i < 8; ++i)
+        {
+            int x_next{x_curr + dx[i]};
+            int y_next{y_curr + dy[i]};
+    
+            if (!inbounds(x_next, y_next)) 
+            {
+                continue;
+            }
+            // Only update if the distance is bigger
+            if (distances[x_next][y_next] > distances[x_curr][y_curr] + 1)
+            {
+                distances[x_next][y_next] = distances[x_curr][y_curr] + 1;
+                q.push(make_pair(x_next, y_next));
+            }
+        }
+    }
+    // Test print
+    // cout << "\n";
+    // cout << "|" << setfill('-') << setw(8*5+1) << "|\n" << setfill(' ');
+    // for (auto x : distances) {
+    //     for (auto y : x) {
+    //         cout << "|" << setw(3) << y << " ";
+    //     }
+    //     cout << "|\n|" << setfill('-') << setw(8*5+1) << "|\n" << setfill(' ');
+    // }
+}
+
+void solve(string start)
+{
+    // Convert starting point to 0-indexed ints
+    int x_start{start[0] - 'a'};
+    int y_start{start[1] - '1'};
+    // cout << "x: " << x_start << " y: " << y_start << endl;
+
+    // Vector containing distances from start to each point
+    vector< vector <int> > distances(8, vector<int>(8, INT_MAX));
+
+    // Initializing direction vectors
+    vector<int> dx{-2, -2, -1, -1, 1, 1, 2, 2};
+    vector<int> dy{-1, 1, -2, 2, -2, 2, -1, 1};
+
+    // Do breath first search
+    bfs(distances, x_start, y_start, dx, dy);
+
+    // Find number of jumps to the hiding places
+    int biggest{0};
+    for (int i{0}; i < 8; ++i)
+    {
+        for (int j{0}; j < 8; ++j)
+        {
+            biggest = max(biggest, distances[i][j]);
+        }
+    }
+    cout << biggest << " ";
+
+    // Print position of hiding places in order from top to bottom and left to right
+    for (int i{7}; i >= 0; --i)
+    {
+        for (int j{0}; j < 8; ++j)
+        {
+            if (distances[j][i] == biggest)
+            {
+                cout << (char)(j + 'a') << (char)(i + '1') << " ";
+            }
+        }
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Read number of test cases
+    int numberOfTestCases{0};
+    cin >> numberOfTestCases;
+
+    // For every test case
+    while(numberOfTestCases--)
+    {
+        // Read input
+        string startPoint{};
+        cin >> startPoint;
+
+        // cout << startPoint << ": ";
+        solve(startPoint);
+    }
+
+    return 0;
+}
