27d79d9803cad2434c9d6503f895f9137e9cafe6
Richard Johansson <ricjo462@student.liu.se>
2022-04-12 22:23:24 +0200

    Working for all cases.

--- excercises/8-StringsI/a-evilstraw.cc
+++ excercises/8-StringsI/a-evilstraw.cc
@@ -1,5 +1,5 @@
 /**
- * @file evilstraw.cc
+ * @file a-evilstraw.cc
  * @author Richard Johansson (ricjo462@student.liu.se)
  */
 #include <iostream>
@@ -9,71 +9,73 @@
 
 using namespace std;
 
-/**
- * @brief Takes a word as an argument, moves the letter at index x to the end and 
- * removes first and last letter from the word, returns number of swaps needed to place
- * the chosen letter last.
- * 
- * @param word word to be rearranged
- * @param x letter at index x to be moved
- * @return int number of swaps needed
- */
-int moveToEnd(string &word, int x)
-{
-    int swaps{0};
-    cout << "word: " << word << ", index x: " << x << ", letter: " << word[x] << "\n";
-
-    while (x < word.length()-1)
-    {
-        // cout << word.substr(0, x) << "\n";
-        // cout << word[x+1] << "\n";
-        // cout << word[x] << "\n";
-        // cout << word.substr(x+2, word.length()-1) << "\n";
-
-        // Swap letter x and x+1
-        word = word.substr(0, x) +  word[x+1] +  word[x] + word.substr(x+2, word.length()-1);
-        cout << "Swap " << swaps << ", New word: " << word << "\n";
-       
-        ++x;
-        ++swaps;
-    }
-    // TODO: Iteration 2 we want to move the letter not to end of word but to end-1
-    word = word.substr(1, word.length()-2);
-
-    cout << "Swaps: " << swaps << "\n";
-
-    return swaps;
-}
-
 void solve(string &word)
 {
-        // Match leftmost char with rightmost mathcing char
-        int leftmost{0}, rightmost{(int)word.length()-1}, swaps{0};
+    // Count occurrences of all characters
+    unordered_map<char, int> charachterMap;
+    for (int i{0}; i < word.length(); ++i)
+    {
+        ++charachterMap[word[i]];
+    }
 
-        char curr;
-        while(leftmost < rightmost)
+    // Count odd occurrences
+    int oddCount{0};
+    char oddChar;
+    for (auto &x : charachterMap)
+    {
+        if (x.second % 2 != 0)
         {
-            curr = word[leftmost];
-            // Test print
-            cout << "leftmost: " << leftmost << " = " << word[leftmost] << ", " 
-                << "rightmost: " << rightmost << " = " << word[rightmost] << ", " 
-                << "curr: " << curr << ", "
-                << "word: " << word << "\n";
-
-            if (curr == word[rightmost])
-            {
-                // Move letter to end of word
-                swaps += moveToEnd(word, rightmost);
-
-                ++leftmost;
-                rightmost = (int)word.length() - leftmost;
-            }
-
-            --rightmost;
+            ++oddCount;
+            oddChar = x.first;
         }
-
-        cout << "swaps: " << swaps << "\n";
-        cout << "----------------------\n";
+    }
+    // If this count is greater than 1 or is equal to 1 and length of the 
+    // string is even then palindrome cannot be formed from the given string
+    if (oddCount > 1 || oddCount == 1 && word.length() % 2 == 0)
+    {
+        cout << "Impossible" << "\n";
+        return;
+    }
+    // Else reorder to create palindrome and count swaps
+    else
+    {
+        int counter{0};
+        for (int i{0}, j{(int)(word.length() - 1)}; i < j; ++i, --j)
+        {
+            int leftmost, rightmost;
+            for (rightmost = j; rightmost > i; --rightmost)
+            {
+                if (word[i] == word[rightmost])
+                {
+                    break;
+                }
+            }
+            for (leftmost = i; leftmost < j; ++leftmost)
+            {
+                if (word[j] == word[leftmost])
+                {
+                    break;
+                }
+            }
+            if (j - rightmost > leftmost - i)
+            {
+                counter += leftmost - i;
+                for (int k = leftmost - 1; k >= i; k--)
+                {
+                    swap(word[k], word[k + 1]);
+                }
+            }
+            else
+            {
+                counter += j - rightmost;
+                for (int k = rightmost + 1; k <= j; k++)
+                {
+                    swap(word[k], word[k - 1]);
+                }
+            }
+        }
+        cout << counter << "\n";
+    }
 }
 
 int main()
@@ -84,20 +86,12 @@ int main()
     // Read number of test cases
     int n;
     cin >> n;
-    // For every test case
-    
+    string word;
+    // Solve for every case
     while(n--)
     {
-        // Read input
-        string word;
         cin >> word;
-
-        // Test print
-        cout << word << "\n";
-
         solve(word);
     }
-    //string word = "aaabbb";
-    //cout << moveToEnd(word, 2) << "\n";
     return 0;
 }
